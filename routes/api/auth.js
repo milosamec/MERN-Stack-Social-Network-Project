49c22d41df587f75ff5a22c6b483f1d5c2e7d538
unknown <miloszz87@gmail.com>
2020-06-28 15:57:56 -0400

    create express server, routed files with express router, created user, profile and post models, implemented auth

---
+++ routes/api/auth.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+
+// @route   GET api/auth
+// @desc    Test route
+// @access  Public
+// Add the auth middleware as a second paramter
+// It makes the route protected
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/aith
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Make a request and send data - we can get it using req.body
+    // We can destructure and pull out stuff
+    const { email, password } = req.body;
+
+    // Try catch - async function return promise
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Check if credentials match (plain and encrypted)
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // If not - return status
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return jsonwebtoken in payload
+
+      const payload = {
+        user: {
+          // With mongoose - we don't need _id
+          id: user.id
+        }
+      };
+      // We have to sign the (token) payload and add a secret inside of config json
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const normalize = require('normalize-url');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Make a request and send data - we can get it using req.body
+    // We can destructure and pull out stuff
+    const { name, email, password } = req.body;
+
+    // Try catch - async function return promise
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar
+
+      const avatar = normalize(
+        gravatar.url(email, {
+          // Size of gravatar
+          s: '200',
+          // Rating
+          r: 'pg',
+          // Default Image (user icon)
+          d: 'mm'
+        })
+      );
+
+      // Create an instance of a User using the user var above and set it
+      // to new user
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt the password using Bcrypt
+
+      const salt = await bcrypt.genSalt(10);
+
+      // We want to take the password and hash it
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user in DB - returns promise -  using async await
+      await user.save();
+
+      // Return jsonwebtoken in payload
+
+      const payload = {
+        user: {
+          // With mongoose - we don't need _id
+          id: user.id
+        }
+      };
+      // We have to sign the (token) payload and add a secret inside of config json
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      res.send({ token });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
